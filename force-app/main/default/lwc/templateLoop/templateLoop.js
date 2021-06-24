e6c50e35a580cc142baa7d854b39aa5d1e3c7962
Manas Datta <manasdatta.cs@gmail.com>
2021-06-25 00:34:32 +0800

    commit 4

---
+++ force-app/main/default/lwc/templateLoop/templateLoop.js
@@ -0,0 +1,40 @@
+import MailingAddress from '@salesforce/schema/Contact.MailingAddress';
+import { LightningElement } from 'lwc';
+
+export default class TemplateLoop extends LightningElement {
+
+carList=["A","B","C","D","E","F","G"]
+
+Companies =[
+
+       {
+        id : 1,
+        Name : "Abott",
+        Company : "ABC"
+       }, 
+
+
+       {
+        id : 2,
+        Name : "Abott2",
+        Company : "ABCD"
+       }, 
+       
+       {
+        id : 3,
+        Name : "Abott3",
+        Company : "ABCDE"
+       }, 
+       
+       {
+        id : 4,
+        Name : "Abott4",
+        Company : "ABCDEF"
+       } 
+
+
+
+]
+
+
+}
