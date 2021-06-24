f13f03c1564eb14f9c6911354e3819b9914cc67a
Manas Datta <manasdatta.cs@gmail.com>
2021-06-24 20:04:23 +0800

    commit 3

--- force-app/main/default/lwc/createConditionalRendering/createConditionalRendering.js
+++ force-app/main/default/lwc/createConditionalRendering/createConditionalRendering.js
@@ -2,20 +2,17 @@ import { LightningElement } from 'lwc';
 
 export default class CreateConditionalRendering extends LightningElement {
     isVisible =false
+    name
     handleClick (event){
         this.isVisible = true
-
-
-
     }
-
-
-
-
-
-
-
-
+        changeHandler(event){
+            this.name = event.target.value
+        }
+    
+        get helloMethod(){
+            return this.name === 'hello'
+        }
 
 }
 
